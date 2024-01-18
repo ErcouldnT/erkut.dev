1e22eb78d032a5e9fe5e91f8014b8de3ac4ce564
Ercode Techoglu <ercode@pm.me>
2024-01-18 20:12:14 +0300

    Initial commit :boom:

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	// Buraya 2048 oyunu yaz.
+</script>
+
+<div class="text-center">
+	<h1 class="text-2xl">404</h1>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	// Highlight JS
+	import hljs from 'highlight.js/lib/core';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	import xml from 'highlight.js/lib/languages/xml'; // for HTML
+	import css from 'highlight.js/lib/languages/css';
+	import javascript from 'highlight.js/lib/languages/javascript';
+	import typescript from 'highlight.js/lib/languages/typescript';
+
+	hljs.registerLanguage('xml', xml); // for HTML
+	hljs.registerLanguage('css', css);
+	hljs.registerLanguage('javascript', javascript);
+	hljs.registerLanguage('typescript', typescript);
+	storeHighlightJs.set(hljs);
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<svelte:head>
+	<title>Erkut TEKOĞLU</title>
+	<meta name="description" content="Full Stack Developer" />
+	<meta name="keywords" content="HTML, CSS, JavaScript" />
+	<meta name="author" content="Ercode Tech" />
+</svelte:head>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<a href="/" class="text-xl">Ercode Tech</a>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://linkedin.com/in/ercodes"
+					target="_blank"
+					rel="noreferrer"
+				>
+					LinkedIn
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/ErcouldnT?tab=repositories"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Projects
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/ErcouldnT"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
